11a662fb64dff077787649f870c6070180c5c7e5
keillyn <keillynbarrios22@gmail.com>
2021-11-18 11:02:26 -0400

    pagina 404 init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Typography, Button } from 'antd';
+import 'antd/dist/antd.css';
+import Pngtree from './assets/img/Pngtree.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {Title}=Typography;
+
+function App(){
+	return(
+		<div className="App">
+		 <br /> 
+		<h1>Opps... page no found!</h1>
+		<img src={Pngtree} />
+        </div>
+	);
+	
 }
 
 export default App;
