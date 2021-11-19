5b2a428857ed31eec2fb24458ac2b03c89a415a9
keillyn <keillynbarrios22@gmail.com>
2021-11-19 10:25:27 -0400

    pagina 404 colores nuevo y boton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { Typography, Button } from 'antd';
 import 'antd/dist/antd.css';
 import Pngtree from './assets/img/Pngtree.png';
+import { WarningOutlined } from '@ant-design/icons';
+import './App.css';
+
 
 const {Title}=Typography;
 
 function App(){
 	return(
 		<div className="App">
-		 <br /> 
-		<h1>Opps... page no found!</h1>
+		 
+		<div className="raya"></div>
 		<img src={Pngtree} />
+		<div className="Descripcion">
+		 		<h1>Ups..<br />La página no ha sido encontrada <WarningOutlined style={{fontSize: '40px', color: 'orange'}} /></h1>
+		 		<h3>Parece que ha habido un error con la página que estabas buscando.<br />
+		 		Es posible que la entrada alla sido eliminada o que la dirección no exista.</h3>
+                <br />
+                <br />
+                <br />
+                <Button type="ghost">Volver a la página principal</Button>
+                <br />
+                <br />
+                <br />
+		 		
+				
+		</div>
         </div>
 	);
 	
